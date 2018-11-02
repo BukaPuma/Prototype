0c76d59af0dda538d081b7e5f04e471a2c1d4aa1
Пользователь <riocuser@Polzovatels-iMac.local>
2018-11-02 08:08:50 -0700

    initial commit

---
+++ FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Money
+//
+//  Created by Пользователь on 02/11/2018.
+//  Copyright © 2018 Пользователь. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
